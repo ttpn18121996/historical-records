383990e34080d98dc5326620abf6b8dabfaa0131
Trinh Tran Phuong Nam <ttpn18121996@gmail.com>
2024-05-28 14:03:34 +0700

    Support custom device_name

--- config/historical-records.php
+++ config/historical-records.php
@@ -2,4 +2,10 @@
 
 return [
     'history_expires' => 90, // days
+    'device_name' => [
+        'phone' => 'phone',
+        'tablet' => 'tablet',
+        'desktop' => 'desktop',
+        'default' => 'unknown',
+    ],
 ];
