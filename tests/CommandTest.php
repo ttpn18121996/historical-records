13de92261ab368464950c237786a5f56b270596b
Trinh Tran Phuong Nam <ttpn18121996@gmail.com>
2024-05-28 13:47:02 +0700

    Fix test

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -20,7 +20,35 @@ class CommandTest extends TestCase
         $historyId = $history->id;
         $history->update(['created_at' => Carbon::now()->subDay()->subMinute()]);
 
-        $this->artisan('historical-records:cleanup', ['--time' => '1d']);
+        $historyRepository->cleanup(1);
+
         $this->assertNull(History::find($historyId));
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function test_it_can_be_clean_up_the_history_with_time_options(string $time, string $method, int $value)
+    {
+        $historyRepository = app(HistoryRepository::class);
+        $user = User::factory()->create();
+        $history = $historyRepository->saveHistory($user->id, 'testing', 'test');
+        $historyId = $history->id;
+        $history->update(['created_at' => Carbon::now()->{$method}($value)->subMinute()]);
+
+        $this->artisan('historical-records:cleanup', ['--time' => $time]);
+
+        $this->assertNull(History::find($historyId));
+    }
+
+    public static function provider()
+    {
+        return [
+            ['1m', 'subMonths', 1],
+            ['1months', 'subMonths', 1],
+            ['1y', 'subYears', 1],
+            ['1years', 'subYears', 1],
+            ['1invalid', 'subDays', 90],
+        ];
     }
 }
